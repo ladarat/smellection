7df108b9eddf4248267f0699ea53171804c0fb21
Ladarat P <ladarat.y@gmail.com>
2016-11-19 15:08:47 +0700

    Replace each Rule with for

--- src/com/industriallogic/collections/fizzbuzz/FizzBuzz.java
+++ src/com/industriallogic/collections/fizzbuzz/FizzBuzz.java
@@ -6,22 +6,19 @@ import java.util.List;
 public class FizzBuzz {
 
 
-    public String answer(int number){
+    public String answer(int number) {
 
         List<Rule> ruleList = new ArrayList<Rule>();
         ruleList.add(new FizzBuzzRule());
         ruleList.add(new FizzRule());
         ruleList.add(new BuzzRule());
 
-        if(ruleList.get(0).check(number)){
-            return ruleList.get(0).answer();
+        for (Rule rule : ruleList) {
+            if (rule.check(number)) {
+                return rule.answer();
+            }
         }
-        if(ruleList.get(1).check(number)){
-            return ruleList.get(1).answer();
-        }
-        if(ruleList.get(2).check(number)){
-            return ruleList.get(2).answer();
-        }
+
         return String.valueOf(number);
     }
 
