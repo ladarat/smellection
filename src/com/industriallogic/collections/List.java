c0159e8db113f0c05baad4c5de9d52199250254b
Ladarat P <ladarat.y@gmail.com>
2016-11-19 11:22:57 +0700

    Remove temp variable

--- src/com/industriallogic/collections/List.java
+++ src/com/industriallogic/collections/List.java
@@ -12,76 +12,78 @@
 package com.industriallogic.collections;
 
 public class List extends AbstractList {
-	private Object[] elements = new Object[10];
-	private int size = 0;
-	private boolean readOnly;
+    private Object[] elements = new Object[10];
+    private int size = 0;
+    private boolean readOnly;
 
-	public boolean isEmpty() {
-		return size == 0;
-	}
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
-	public void add(Object element) {
-		if (!readOnly) {
-			int newSize = size + 1;
-			if (newSize > elements.length) {
-				Object[] newElements =
-					new Object[elements.length + 10];
-				for (int i = 0; i < size; i++)
-					newElements[i] = elements[i];
-				elements = newElements;
-			}
-			elements[size++] = element;
-		}
-	}
+    public void add(Object element) {
+        if (readOnly) {
+            return;
+        }
+//        int newSize = size + 1;
+        if (size + 1 > elements.length) {
+            Object[] newElements =
+                    new Object[elements.length + 10];
+            for (int i = 0; i < size; i++)
+                newElements[i] = elements[i];
+            elements = newElements;
+        }
+        elements[size++] = element;
 
-	public boolean contains(Object element) {
-		for (int i=0; i<size; i++) 
-			if (elements[i].equals(element))
-				return true;
-		return false;
-	}
+    }
 
-	public int size() {
-		return size;
-	}
+    public boolean contains(Object element) {
+        for (int i = 0; i < size; i++)
+            if (elements[i].equals(element))
+                return true;
+        return false;
+    }
 
-	public boolean remove(Object element) {
-		if (readOnly)
-			return false;
-		else 	
-			for (int i = 0; i < size; i++)
-				if (elements[i].equals(element)) {
-					elements[i] = null;
-					Object[] newElements = new Object[size - 1];
-					int k = 0;
-					for (int j = 0; j < size; j++) {
-						if (elements[j] != null)
-							newElements[k++] = elements[j];
-					}
-					size--;
-					elements = newElements;
-					return true;
-				}
-		return false;
-	}
-	
-	public Object get(int i) {
-		return elements[i];
-	}
+    public int size() {
+        return size;
+    }
 
-	public int capacity() {
-		return elements.length;
-	}
+    public boolean remove(Object element) {
+        if (readOnly)
+            return false;
+        else
+            for (int i = 0; i < size; i++)
+                if (elements[i].equals(element)) {
+                    elements[i] = null;
+                    Object[] newElements = new Object[size - 1];
+                    int k = 0;
+                    for (int j = 0; j < size; j++) {
+                        if (elements[j] != null)
+                            newElements[k++] = elements[j];
+                    }
+                    size--;
+                    elements = newElements;
+                    return true;
+                }
+        return false;
+    }
 
-	public void set(int i, Object value) {
-		if (!readOnly) {
-			if (i >= size)
-				throw new ArrayIndexOutOfBoundsException();
-			elements[i] = value;
-		}
-	}
+    public Object get(int i) {
+        return elements[i];
+    }
 
-	public void setReadOnly(boolean b) {
-		readOnly = b;
-	}
+    public int capacity() {
+        return elements.length;
+    }
+
+    public void set(int i, Object value) {
+        if (!readOnly) {
+            if (i >= size)
+                throw new ArrayIndexOutOfBoundsException();
+            elements[i] = value;
+        }
+    }
+
+    public void setReadOnly(boolean b) {
+        readOnly = b;
+    }
 }
